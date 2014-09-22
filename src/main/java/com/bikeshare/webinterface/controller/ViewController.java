59144d8884a152c8a3c86ea8f2bf8c47d56f9d75
Cavanaugh, Sean <Cavanaugh.Sean@principal.com>
2014-09-22 14:27:40 -0500

    Setting up web project

---
+++ src/main/java/com/bikeshare/webinterface/controller/ViewController.java
@@ -0,0 +1,17 @@
+package com.bikeshare.webinterface.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping(value = "/view")
+public class ViewController {
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(Model model){
+		return "index.jsp";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="../resources/css/main.css" />
+</head>
+<body>
+	<script src="../resources/js/index.js"></script>
+	RAWR
+</body>
+</html>
