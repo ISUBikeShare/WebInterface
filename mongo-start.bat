2405097e39685bf6032af09100716b4ccc95f676
seancavanaugh <cavanaughmsean@gmail.com>
2015-03-04 21:37:57 -0600

    Removing unused file

--- mongo-start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo —————————————–
-echo . Starting the MongoDB instance
-echo —————————————–
-start C:\MongoDB\bin\mongod.exe 
-start C:\MongoDB\bin\mongo.exe
-start cmd /k "cd C:\MongoDB\bin"
-exit
